29f3d709877d1b021c30e0959b3e362db022a687
Alesh Houdek <mail@alesh.com>
2026-07-20 14:50:43 -0600

    Format filesize and date

--- src/FileList.tsx
+++ src/FileList.tsx
@@ -13,14 +13,27 @@ interface FileNode {
 
 type FileSystemNode = DirectoryNode | FileNode;
 
+function formatFileSize(input: number): string {
+  const units = ["bytes", "kb", "mb", "gb", "tb"];
+
+  for (const unit of units) {
+    if (input < 1000 || unit === "tb") {
+      return Math.round(input * 100) / 100 + " " + unit;
+    }
+    input = input / 1000;
+  }
+}
+
 function FileList({ files }: { files: FileSystemNode[] }) {
   return (
     <div className="files">
       <table>
         <thead>
-          <th>Name</th>
-          <th>Size</th>
-          <th>Modified</th>
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+            <th>Modified</th>
+          </tr>
         </thead>
         <tbody>
           {files.map((item) => (
@@ -28,8 +41,8 @@ function FileList({ files }: { files: FileSystemNode[] }) {
               <td>{item.name}</td>
               {item.type === "file" ? (
                 <>
-                  <td>{item.size}</td>
-                  <td>{item.modified}</td>
+                  <td>{formatFileSize(item.size)}</td>
+                  <td>{new Date(item.modified).toLocaleDateString()}</td>
                 </>
               ) : (
                 <>
